f6df2794d8618852b51b39a67027a6d00d095eaa
Vijay <136799871+Vijaykaruna@users.noreply.github.com>
2026-01-26 17:20:24 +0530

    Update project

--- src/components/project.list.js
+++ src/components/project.list.js
@@ -21,7 +21,7 @@ import tempQuiz from "../assets/image/img-quiz.jpg";
       img: guestie,
       desc: "This website organizated for hotel management system. By using time reduce and order the foods also track the foods details. The Rating system to knows the review in the hotel management.",
       link: "https://smart-hotel-food-ordering-system-1.onrender.com",
-      repo: "https://github.com/Vijaykaruna/Ecom_Guestie",
+      repo: "https://github.com/Vijaykaruna/Smart_Hotel_Food_Ordering_System",
     },
     {
       id: "todo",
